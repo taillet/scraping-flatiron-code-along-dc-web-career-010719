0e7f2be2a1e5fbf0e15e9f1855a5b3d26a3fa9a2
Heloise Taillet <heloise96@hotmail.com>
2019-01-14 00:37:51 -0500

    all tests pass

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,11 +1,10 @@
 require 'nokogiri'
 require 'open-uri'
-
 require_relative './course.rb'
+require 'pry'
 
 class Scraper
-  
-    def print_courses
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title
@@ -15,8 +14,24 @@ class Scraper
       end
     end
   end
-  
+
+  def get_page
+    html = open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
+    doc = Nokogiri::HTML(html)
+  end
+
+  def get_courses
+    doc = get_page
+    return doc.css("article.post")
+  end
+
+  def make_courses
+    doc = get_courses
+    doc.each do |element|
+      course = Course.new
+      course.title = element.css("h2").text
+      course.schedule = element.css("em.date").text
+      course.description = element.css("p").text
+    end
+  end
 end
-
-
-
